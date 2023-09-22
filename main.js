b875c3dd59851db9c62a11d6dfcdf7a05b5054f2
Kush Brahmbhatt <kushbrahmbhatt23@gmail.com>
2023-09-22 20:56:42 +0530

    Updated main.js
    Added APIs in main.js
    
    apiKey and apiUrl

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 const apiKey = "deda89a9d5943d80a12b75e1c9835446";
-const apiUrl = "";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Ahmedabad";
+//
+async function checkWeather() {
+  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+  var data = await response.json();
+
+  console.log(data);
+}
+
+checkWeather();
+
+// const apiKey = "deda89a9d5943d80a12b75e1c9835446";
