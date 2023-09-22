628dd5233bd1e298052999ed6d0104ff3d24ab73
Kush Brahmbhatt <kushbrahmbhatt23@gmail.com>
2023-09-22 21:32:05 +0530

    Updated index.html, main.js and style.css
    Updated index.html, main.js and style.css

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 const apiKey = "deda89a9d5943d80a12b75e1c9835446";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Ahmedabad";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Surat";
 //
 async function checkWeather() {
   const response = await fetch(apiUrl + `&appid=${apiKey}`);
   var data = await response.json();
 
   console.log(data);
+
+  document.querySelector(".weather_city").innerHTML = data.name;
+  document.querySelector(".weather_temperature").innerHTML =
+    Math.round(data.main.temp) + " °C";
+  document.querySelector(".weather_humidity").innerHTML =
+    data.main.humidity + "%";
+  document.querySelector(".weather_wind").innerHTML =
+    Math.round(data.wind.speed) + " Km/h";
+  document.querySelector(".weather_wind_deg_num").innerHTML =
+    Math.round(data.wind.deg) + " °C";
+  document.querySelector(".weather_temp_max_num").innerHTML =
+    Math.round(data.main.temp_max) + " °C";
+  document.querySelector(".weather_temp_min_num").innerHTML =
+    Math.round(data.main.temp_min) + " °C";
+  document.querySelector(".weather_pressure").innerHTML = data.main.pressure;
 }
 
 checkWeather();
